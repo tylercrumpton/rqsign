2a52911cea5287fd01cb67b968562afb45c5ad53
Tyler Crumpton <tyler.crumpton@gmail.com>
2015-07-09 23:54:57 -0500

    Move some pieces around in preparation for message parsing.

--- sign.py
+++ sign.py
@@ -1,13 +1,20 @@
 import couchdb
 import ConfigParser
 
-config = ConfigParser.ConfigParser()
-config.read("rqsettings.ini")
+def main():
+    config = ConfigParser.ConfigParser()
+    config.read("rqsettings.ini")
 
-server = couchdb.Server(url=config.get("Server", "url"))
-message_db = server[config.get("Server", "message_db")]
-project_name = config.get("Project", "name")
+    server = couchdb.Server(url=config.get("Server", "url"))
+    message_db = server[config.get("Server", "message_db")]
+    project_name = config.get("Project", "name")
 
-for change in message_db.changes(feed='continuous', since='now', heartbeat=1000, filter='project/by_name',
-                                 name=project_name, include_docs=True):
-    print change['doc']
+    for change in message_db.changes(feed='continuous', since='now', heartbeat=1000, filter='project/by_name',
+                                     name=project_name, include_docs=True):
+        parse_message(change['doc'])
+
+def parse_message(message):
+    print message
+
+if __name__ == "__main__":
+    main()
