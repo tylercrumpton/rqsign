405d1a29a804da8707e0f94ee537cb5e79d3794f
Tyler Crumpton <tyler.crumpton@gmail.com>
2015-07-09 23:42:46 -0500

    Watch couch changes feed for project.

--- sign.py
+++ sign.py
@@ -5,3 +5,9 @@ config = ConfigParser.ConfigParser()
 config.read("rqsettings.ini")
 
 server = couchdb.Server(url=config.get("Server", "url"))
+message_db = server[config.get("Server", "message_db")]
+project_name = config.get("Project", "name")
+
+for change in message_db.changes(feed='continuous', since='now', heartbeat=1000, filter='project/by_name',
+                                 name=project_name, include_docs=True):
+    print change['doc']
